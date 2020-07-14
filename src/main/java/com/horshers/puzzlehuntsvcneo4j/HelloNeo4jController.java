3276d45d8eb54fe4d2d985710c344253e3943d53
Greg Katz <greg@madeofmeat.org>
2020-07-14 10:26:23 -0700

    Add a stubby RestController

---
+++ src/main/java/com/horshers/puzzlehuntsvcneo4j/HelloNeo4jController.java
@@ -0,0 +1,13 @@
+package com.horshers.puzzlehuntsvcneo4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloNeo4jController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello";
+  }
+}
