cb58a112eac055401c39f6c120cad607133ae26b
Greg Katz <greg@madeofmeat.org>
2020-07-14 09:34:10 -0700

    Initial commit of Spring Initializr-generated project plus Neo4j data loader script

---
+++ src/main/java/com/horshers/puzzlehuntsvcneo4j/PuzzlehuntServiceNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.horshers.puzzlehuntsvcneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlehuntServiceNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlehuntServiceNeo4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horshers/puzzlehuntsvcneo4j/PuzzlehuntServiceNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horshers.puzzlehuntsvcneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlehuntServiceNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
