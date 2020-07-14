95793b181bafc231ff56b42b42b1119557080af1
MikeW <zikfat@gmail.com>
2020-07-14 13:30:50 -0700

    Leaderboard and TeamResult

---
+++ src/main/java/com/horshers/puzzlehuntsvcneo4j/model/Leaderboard.java
@@ -0,0 +1,7 @@
+package com.horshers.puzzlehuntsvcneo4j.model;
+
+import java.util.List;
+
+public class Leaderboard {
+  List<TeamResult> teamResults;
+}
---
+++ src/main/java/com/horshers/puzzlehuntsvcneo4j/model/TeamResult.java
@@ -0,0 +1,12 @@
+package com.horshers.puzzlehuntsvcneo4j.model;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class TeamResult {
+  String name;
+  int points;
+  Duration duration;
+  Instant start;
+  Instant end;
+}
