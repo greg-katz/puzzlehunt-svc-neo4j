bae29e0112cf8867c029497c0bf42f4534af9e8c
Greg Katz <greg@madeofmeat.org>
2020-07-14 12:11:07 -0700

    Run "match (n) return n" Neo4j query and return a list of each node's labels

--- src/main/java/com/horshers/puzzlehuntsvcneo4j/HelloNeo4jController.java
+++ src/main/java/com/horshers/puzzlehuntsvcneo4j/HelloNeo4jController.java
@@ -1,13 +1,23 @@
 package com.horshers.puzzlehuntsvcneo4j;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloNeo4jController {
 
   @GetMapping("/hello")
-  public String hello() {
-    return "Hello, Neo4j!";
+  public List<String> hello() {
+    Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+    Session session = driver.session();
+    Result result = session.run("match (n) return n");
+    return result.list(r -> r.get("n").asNode().labels().toString());
   }
 }
