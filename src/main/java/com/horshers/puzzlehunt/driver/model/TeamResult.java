5681361a72868d5ed95c49f4ff030bed8a27adf6
Greg Katz <greg@madeofmeat.org>
2020-07-15 13:23:34 -0700

    Remove unused OGM-related code from TeamResult (because it doesn't use OGM)

--- src/main/java/com/horshers/puzzlehunt/driver/model/TeamResult.java
+++ src/main/java/com/horshers/puzzlehunt/driver/model/TeamResult.java
@@ -1,15 +1,9 @@
 package com.horshers.puzzlehunt.driver.model;
 
 import lombok.Data;
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
-import org.neo4j.ogm.id.UuidStrategy;
 
 @Data
 public class TeamResult {
-  @Id @GeneratedValue(strategy = UuidStrategy.class)
-  long id;
-
   private String name;
   private boolean finished;
   private int score;
