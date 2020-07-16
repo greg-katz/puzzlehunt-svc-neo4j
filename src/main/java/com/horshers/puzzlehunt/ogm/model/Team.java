8f956c9d7f711728916b5d825cc80c3e8e4c99e7
MikeW <zikfat@gmail.com>
2020-07-16 13:27:04 -0700

    Team ignore its persons to prevent circular reference.

--- src/main/java/com/horshers/puzzlehunt/ogm/model/Team.java
+++ src/main/java/com/horshers/puzzlehunt/ogm/model/Team.java
@@ -1,6 +1,8 @@
 package com.horshers.puzzlehunt.ogm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.ToString;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
@@ -14,6 +16,8 @@ public class Team extends Entity {
 
   String name;
 
+  @ToString.Exclude
+  @JsonIgnore
   @Relationship(type = "MEMBER_OF", direction = INCOMING)
   List<Person> players;
 }
