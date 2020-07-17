86202cb19990f0ea43db3f4d587321d8967624c7
Greg Katz <greg@madeofmeat.org>
2020-07-16 18:25:00 -0700

    Change TeamSolvedPuzzle's start and end fields from Instants to LocalTimes to match the Neo4j SOLVED relationship properties

--- src/main/java/com/horshers/puzzlehunt/springdata/model/TeamSolvedPuzzle.java
+++ src/main/java/com/horshers/puzzlehunt/springdata/model/TeamSolvedPuzzle.java
@@ -1,20 +1,25 @@
 package com.horshers.puzzlehunt.springdata.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 import org.neo4j.ogm.annotation.EndNode;
 import org.neo4j.ogm.annotation.RelationshipEntity;
 import org.neo4j.ogm.annotation.StartNode;
 
-import java.time.Instant;
+import java.time.LocalTime;
 
 @RelationshipEntity(type = "SOLVED")
+@Data
 public class TeamSolvedPuzzle {
 
+  @JsonIgnore
   @StartNode
   Team team;
 
+  @JsonIgnore
   @EndNode
   Puzzle puzzle;
 
-  Instant start;
-  Instant end;
+  LocalTime start;
+  LocalTime end;
 }
