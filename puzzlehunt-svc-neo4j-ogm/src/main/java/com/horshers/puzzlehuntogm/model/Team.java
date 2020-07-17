8d872f2a5f7770aeec38d72262a7822042c1cd84
Greg Katz <greg@madeofmeat.org>
2020-07-17 11:00:30 -0700

    Add @EqualsAndHashCode(callSuper = true) to Team in the OGM module

--- puzzlehunt-svc-neo4j-ogm/src/main/java/com/horshers/puzzlehuntogm/model/Team.java
+++ puzzlehunt-svc-neo4j-ogm/src/main/java/com/horshers/puzzlehuntogm/model/Team.java
@@ -1,6 +1,7 @@
 package com.horshers.puzzlehuntogm.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
@@ -10,6 +11,7 @@ import static org.neo4j.ogm.annotation.Relationship.INCOMING;
 
 @NodeEntity
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class Team extends Entity {
 
   String name;
