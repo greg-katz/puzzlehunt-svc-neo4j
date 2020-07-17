cc95966e47b53e4ed8fd403b22b480d24b22398b
Greg Katz <greg@madeofmeat.org>
2020-07-17 15:20:02 -0700

    Avert the Whitespapocalypse in Spring Data OgmSessionFactoryConfig

--- puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/config/OgmSessionFactoryConfig.java
+++ puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/config/OgmSessionFactoryConfig.java
@@ -12,9 +12,9 @@ public class OgmSessionFactoryConfig {
   @Bean
   SessionFactory sessionFactory() {
     org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration.Builder()
-        .uri("bolt://localhost:7687")
-        .credentials("neo4j", "password")
-        .build();
+      .uri("bolt://localhost:7687")
+      .credentials("neo4j", "password")
+      .build();
 
     return new SessionFactory(configuration, "com.horshers.puzzlehuntspringdata", "com.horshers.puzzlehuntogm");
   }
