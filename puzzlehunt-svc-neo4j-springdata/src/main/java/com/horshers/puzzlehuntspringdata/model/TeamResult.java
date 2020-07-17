9d18767387df949e4695e6bfb145d091e9d29983
Greg Katz <greg@madeofmeat.org>
2020-07-17 15:18:09 -0700

    Convert the Spring Data TeamResult's time field's value from an IsoDuration object to a human-friendly formatted string

--- puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/model/TeamResult.java
+++ puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/model/TeamResult.java
@@ -2,14 +2,34 @@ package com.horshers.puzzlehuntspringdata.model;
 
 import lombok.Data;
 import org.neo4j.driver.types.IsoDuration;
+import org.neo4j.ogm.annotation.typeconversion.Convert;
+import org.neo4j.ogm.typeconversion.AttributeConverter;
 import org.springframework.data.neo4j.annotation.QueryResult;
+
+import java.time.Duration;
 
 @Data
 @QueryResult
 public class TeamResult {
+
   private String name;
   private boolean finished;
   private int score;
-  // TODO: This is supposed to be a formatted duration
-  private IsoDuration time;
+
+  @Convert(IsoDurationConverter.class)
+  private String time;
+
+  public static class IsoDurationConverter implements AttributeConverter<String, IsoDuration> {
+
+    @Override
+    public IsoDuration toGraphProperty(String value) {
+      return null;
+    }
+
+    @Override
+    public String toEntityAttribute(IsoDuration value) {
+      Duration time = Duration.ofSeconds(value.seconds(), value.nanoseconds());
+      return String.format("%d:%02d:%02d", time.toHoursPart(), time.toMinutesPart(), time.toSecondsPart());
+    }
+  }
 }
