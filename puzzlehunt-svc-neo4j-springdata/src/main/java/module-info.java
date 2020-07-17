f2a77bd06b1137639de56659195e9abc905f6598
MikeW <zikfat@gmail.com>
2020-07-17 13:45:38 -0700

    Adding some more modules

--- puzzlehunt-svc-neo4j-springdata/src/main/java/module-info.java
+++ puzzlehunt-svc-neo4j-springdata/src/main/java/module-info.java
@@ -4,8 +4,10 @@ open module puzzlehunt.svc.neo4j.springdata {
   requires org.neo4j.ogm.core;
   requires org.neo4j.driver;
   requires org.neo4j.ogm.drivers.api;
+  requires spring.tx;
   requires spring.web;
   requires spring.beans;
+  requires spring.data.commons;
   requires lombok;
   requires com.fasterxml.jackson.annotation;
   requires org.apache.commons.collections4;
