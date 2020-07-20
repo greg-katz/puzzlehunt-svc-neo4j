6fb717fe97aa1dc374c70cebd47c1da24d477287
Greg Katz <greg@madeofmeat.org>
2020-07-20 10:30:32 -0700

    Remove unused "find by hunt" methods from TeamRepository because it they cause a startup error now that Team doesn't have a relationship to Hunt

--- puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/repo/TeamRepository.java
+++ puzzlehunt-svc-neo4j-springdata/src/main/java/com/horshers/puzzlehuntspringdata/repo/TeamRepository.java
@@ -1,6 +1,5 @@
 package com.horshers.puzzlehuntspringdata.repo;
 
-import com.horshers.puzzlehuntspringdata.model.Hunt;
 import com.horshers.puzzlehuntspringdata.model.Team;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Component;
@@ -9,12 +8,6 @@ import java.util.UUID;
 
 @Component
 public interface TeamRepository extends Neo4jRepository<Team, UUID> {
-
-  Team findAllByHunt(Hunt hunt);
-
-  Team findByHuntAndName(Hunt hunt, String name);
-
-  Team findByHuntAndName(UUID hunt, String name);
 
   Team findByName(String name);
 }
