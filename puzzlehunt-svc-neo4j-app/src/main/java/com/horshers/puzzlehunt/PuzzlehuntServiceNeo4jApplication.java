2403207e923c619cb2289dcbc1949102cf231291
MikeW <zikfat@gmail.com>
2020-07-28 13:56:45 -0700

    Whoops. Fixing the app's package

--- puzzlehunt-svc-neo4j-app/src/main/java/com/horshers/puzzlehunt/PuzzlehuntServiceNeo4jApplication.java
+++ puzzlehunt-svc-neo4j-app/src/main/java/com/horshers/puzzlehunt/PuzzlehuntServiceNeo4jApplication.java
@@ -1,4 +1,4 @@
-package com.horshers.puzzlehunt.graphqljava;
+package com.horshers.puzzlehunt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
