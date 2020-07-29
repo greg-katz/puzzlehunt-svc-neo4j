cc486bc288834df35110d23f8a406b2735a5f5da
MikeW <zikfat@gmail.com>
2020-07-29 11:03:40 -0700

    Cypher query logging for the bare neo driver.

--- puzzlehunt-svc-neo4j-driver/src/main/java/com/horshers/puzzlehuntdriver/config/NeoDriverConfig.java
+++ puzzlehunt-svc-neo4j-driver/src/main/java/com/horshers/puzzlehuntdriver/config/NeoDriverConfig.java
@@ -1,8 +1,10 @@
 package com.horshers.puzzlehuntdriver.config;
 
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Logging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,7 @@ public class NeoDriverConfig {
 
   @Bean("neo-driver")
   Driver driver() {
-    return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+    Config config = Config.builder().withLogging(Logging.slf4j()).build();
+    return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"), config);
   }
 }
--- puzzlehunt-svc-neo4j-graphql/src/main/java/com/horshers/puzzlehuntgraphql/config/GraphQLConfig.java
+++ puzzlehunt-svc-neo4j-graphql/src/main/java/com/horshers/puzzlehuntgraphql/config/GraphQLConfig.java
@@ -5,12 +5,15 @@ import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 import lombok.SneakyThrows;
 import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Logging;
 import org.neo4j.graphql.SchemaBuilder;
 import org.neo4j.graphql.Translator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.logging.Level;
 
 import javax.annotation.PostConstruct;
 import java.net.URL;
@@ -36,7 +39,8 @@ public class GraphQLConfig {
 
   @Bean("graphql-driver")
   Driver driver() {
-    return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+    Config config = Config.builder().withLogging(Logging.slf4j()).build();
+    return GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"), config);
   }
 
   //--------------------------------------
