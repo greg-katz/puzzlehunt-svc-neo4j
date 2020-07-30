66614309161697716b77d735149fe5e646eeee7b
MikeW <zikfat@gmail.com>
2020-07-30 12:24:56 -0700

    GraphQLController changes for better return types and allowing for multiple queries at once.

--- puzzlehunt-svc-neo4j-graphql/src/main/java/com/horshers/puzzlehuntgraphql/controller/GraphQLController.java
+++ puzzlehunt-svc-neo4j-graphql/src/main/java/com/horshers/puzzlehuntgraphql/controller/GraphQLController.java
@@ -2,6 +2,9 @@ package com.horshers.puzzlehuntgraphql.controller;
 
 import com.horshers.puzzlehuntgraphql.model.GraphQLRequest;
 import graphql.GraphQL;
+import graphql.schema.GraphQLList;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLType;
 import lombok.SneakyThrows;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Record;
@@ -15,8 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -45,22 +53,85 @@ public class GraphQLController {
     // Amazing magic occurs here! The GraphQL query is translated into a single Cypher query, which means that instead
     // of having to make a lot of database queries to fetch individual node properties, we can execute a single query
     // in a single round trip. Bow down before the mighty Translator.
-    Cypher cypher = translator.translate(request.getQuery()).get(0);
+    List<Cypher> queries = translator.translate(request.getQuery());
 
-    // The generated cypher queries will use variables instead of literals even for values that are literal in the
-    // graphQL query. When it does this the variables it generated are in the cypher object that comes back from the
-    // translator. We need to make sure when we run the cypher query we have all the variables that the translator
-    // defined AND all the ones that were specified in the graphQL request itself.
-    Map<String, Object> cypherVariables = new HashMap<>(cypher.getParams());
-    if (request.getVariables() != null) {
-      cypherVariables.putAll(request.getVariables());
-    }
-
-    String cypherString = cypher.getQuery();
-
+    Map<String, Object> dataMap = new HashMap<>();
     try (Session session = driver.session()) {
-      Result result = session.run(cypherString, cypherVariables);
-      return Map.of("data", result.stream().map(Record::asMap).collect(toList()));
+      for (Cypher cypher : queries) {
+        // The generated cypher queries will use variables instead of literals even for values that are literal in the
+        // graphQL query. When it does this the variables it generated are in the cypher object that comes back from the
+        // translator. We need to make sure when we run the cypher query we have all the variables that the translator
+        // defined AND all the ones that were specified in the graphQL request itself.
+        ResultType resultType = getResultType(cypher.getType());
+        Map<String, Object> cypherVariables = new HashMap<>(cypher.getParams());
+        if (request.getVariables() != null) {
+          cypherVariables.putAll(request.getVariables());
+        }
+
+        String cypherString = cypher.getQuery();
+
+        Result result = session.run(cypherString, cypherVariables);
+        if (!result.hasNext()) {
+          Pattern pattern = Pattern.compile("^.*\\s+AS\\s+(.*)\\s*$");
+          Matcher matcher = pattern.matcher(cypherString);
+          if (matcher.matches()) {
+            if (resultType == ResultType.SINGLE) {
+              dataMap.put(matcher.group(1), new HashMap<>());
+            }
+            else if (resultType == ResultType.LIST) {
+              dataMap.put(matcher.group(1), new ArrayList<>());
+            }
+          }
+        }
+        else if (resultType == ResultType.SINGLE) {
+          dataMap.putAll(result.single().asMap());
+        }
+        else if (resultType == ResultType.LIST) {
+          Map<String, Object> listResultMap = new HashMap<>();
+          List<Object> listResult = new ArrayList<>();
+          while (result.hasNext()) {
+            Map<String, Object> currentMap = result.next().asMap();
+            String key = currentMap.keySet().stream().findFirst().get();
+            if (!listResultMap.containsKey(key)) {
+              listResultMap.put(key, listResult);
+            }
+            listResult.add(currentMap.get(key));
+          }
+          dataMap.putAll(listResultMap);
+        }
+      }
     }
+    /*
+    If there are multiple queries in the same batch:
+      * Generate names for each query based on their return value (unless they already have a name).
+      * Add each query result to the data map under a key of its name
+
+    For each query:
+      * If its return type is a list, wrap it with a list even if it only returns a single match
+      * If its return type is not a list, just add it to the result as-is.
+   */
+
+    return Map.of("data", dataMap);
+  }
+
+  ResultType getResultType(GraphQLType type) {
+    if (type == null) {
+      return ResultType.NONE;
+    }
+    else if (type instanceof GraphQLList) {
+      return ResultType.LIST;
+    }
+    else if (type instanceof GraphQLObjectType) {
+      return ResultType.SINGLE;
+    }
+    else {
+      return getResultType(type.getChildren().get(0));
+    }
+  }
+
+  enum ResultType {
+    NONE,
+    SINGLE,
+    LIST
   }
 }
